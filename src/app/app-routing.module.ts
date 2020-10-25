f7876f142a04c374409f79f760a63f34bc8d4f9c
joav <jim.acero.valcarcel@gmail.com>
2020-10-24 20:14:51 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoansComponent } from './loans/loans.component';
+import { PersonsComponent } from './persons/persons.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { WalletComponent } from './wallet/wallet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent
+	},
+	{
+		path: 'wallet',
+		component: WalletComponent
+	},
+	{
+		path: 'transaction',
+		component: TransactionComponent
+	},
+	{
+		path: 'persons',
+		component: PersonsComponent
+	},
+	{
+		path: 'loans',
+		component: LoansComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
